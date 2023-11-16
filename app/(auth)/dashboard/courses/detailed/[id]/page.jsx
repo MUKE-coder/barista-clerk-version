fa2e42ae576a70b3cfde15333d92c673fb0a7be5
mosespace <kisakyemoses50@gmail.com>
2023-11-16 15:27:07 +0300

    feat: added course preview component

--- app/(auth)/dashboard/courses/detailed/[id]/page.jsx
+++ app/(auth)/dashboard/courses/detailed/[id]/page.jsx
@@ -1,9 +1,19 @@
+"use client";
 import React from "react";
+import ReactPlayer from "react-player/youtube";
 
 export default function CourseDetail({ params: { id } }) {
   return (
-    <div>
-      <h2> Course Detail Page</h2>
+    <div className='flex flex-col items-center h-screen gap-8'>
+      <h2 className='font-bold text-4xl uppercase'> video preview</h2>
+      <div className='bg-red-700 h-[70%] w-full'>
+        <ReactPlayer
+          height='600px'
+          width='1200px'
+          className='rounded-2xl'
+          url='https://www.youtube.com/watch?v=ZxjNa_Ief7U'
+        />
+      </div>
     </div>
   );
 }
