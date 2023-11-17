3e9de50a16217cdbd8bdbd631185e52564e2314b
collin kooza <koozacolline1@gmail.com>
2023-11-17 09:52:46 +0300

    collinz new cahnges

--- components/admin/BrowseCourseList.jsx
+++ components/admin/BrowseCourseList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import BrowseCourseCard from "./BrowseCourseCard";
 
-export default function BrowseCourseList() {
+export default function BrowseCourseList({getCourses}) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-2 py-8 gap-4">
-      <BrowseCourseCard />
-      <BrowseCourseCard />
-      <BrowseCourseCard />
-      <BrowseCourseCard />
+      {
+        getCourses.map((course , i)=>{
+          return(
+            <BrowseCourseCard  course={course}/>
+          )
+        })
+      }
+     
     </div>
   );
 }
