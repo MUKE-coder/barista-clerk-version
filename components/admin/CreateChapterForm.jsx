e4f23d09405ab873d7b1c5f777799bcb44f326f5
mosespace <kisakyemoses50@gmail.com>
2023-11-19 22:06:04 +0300

    feat: added base url

--- components/admin/CreateChapterForm.jsx
+++ components/admin/CreateChapterForm.jsx
@@ -61,6 +61,7 @@ export default function CreateChapterForm({
   //     // console.log(error);
   //   }
   // }
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
   async function handleEdit(data) {
     setLoading(true);
